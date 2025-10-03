560c871b08bd95a7426e8943dd77e91bc7e2f61e
pihlk <pihlk@users.noreply.github.com>
2025-10-03 08:58:19 +0000

    deploy: c2f7d8fc778dee5aa57bbfeb434a7697edf1612d

---
+++ v1.1.1/d1/d3f/structwaps__prot__t.js
@@ -0,0 +1,9 @@
+var structwaps__prot__t =
+[
+    [ "flush_hw", "d1/d3f/structwaps__prot__t.html#aa499cc1777013939e37d9d5f70d89e50", null ],
+    [ "frame_removed", "d1/d3f/structwaps__prot__t.html#a61c65b65a33429dfbe882bfa8276dcb5", null ],
+    [ "process_response", "d1/d3f/structwaps__prot__t.html#a4d51a51df284cf5993bcfee4e9a228f7", null ],
+    [ "send_reply", "d1/d3f/structwaps__prot__t.html#a4d03a1e569d586736c439d4dac03726c", null ],
+    [ "update_irq", "d1/d3f/structwaps__prot__t.html#abdd32001645fb37c373cdc186c546567", null ],
+    [ "write_hw", "d1/d3f/structwaps__prot__t.html#aae991e5244689ec5d21ed30ba63022be", null ]
+];
---
+++ v1.1.1/index.js
@@ -0,0 +1,11 @@
+var index =
+[
+    [ "Application", "index.html#application", null ],
+    [ "Wirepas Mesh Single MCU API", "index.html#single_mcu_api", null ],
+    [ "SDK libraries", "index.html#sdk_libraries", null ],
+    [ "Application-specific Hardware Abstraction Layer (HAL)", "index.html#sdk_hal", null ],
+    [ "Wirepas Mesh Stack", "index.html#wirepas_firmware", null ],
+    [ "Wirepas Mesh Bootloader", "index.html#bootloader", null ],
+    [ "Custom bootloader extension", "index.html#bootloader_extension", null ],
+    [ "The physical Hardware", "index.html#hardware", null ]
+];
